8a8d98ece66302c1494da556928526e8c8023ef5
s4lvi <salvicomputers@gmail.com>
2024-11-09 20:22:30 -0500

    update server

--- backend/routes/rsvp.js
+++ backend/routes/rsvp.js
@@ -23,7 +23,6 @@ router.post("/:eventId", async (req, res) => {
       // If user is logged in
       const token = req.header("Authorization").split(" ")[1];
       const decoded = jwt.verify(token, process.env.JWT_SECRET).user;
-      console.log("decoded", decoded);
       const user = await User.findById(decoded._id);
       if (user) {
         rsvpData.user = user._id;
